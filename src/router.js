a82fb5e19a3c1b680bcf26afcf2f769e0dd75566
TaiNguyen073 <75308712+TaiNguyen073@users.noreply.github.com>
2021-05-24 16:57:30 +0700

    add return function.

--- src/router.js
+++ src/router.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 import Dashboard from "./Components/MainInput/Dashboard";
 import {PrivateRoute} from "./Components/MainInput/PrivateRoute";
 import ScrollToTop from "./Components/MainInput/ScrollToTop";
 import Hello from "./Components/Hello";
 import Login from "./Components/login";
-export default props => (
-    <BrowserRouter>
+import {render} from "@testing-library/react";
+
+const Rounter = () => {
+    render()
+    return <BrowserRouter>
         <ScrollToTop>
             <Switch>
-                <PrivateRoute exact path='/' component={ Dashboard } />
-                <Route exact path='/hello' component= {Hello} />
-                <Route exact path='/login' component= {Login} />
+                <PrivateRoute exact path='/' component={Dashboard}/>
+                <Route exact path='/hello' component={Hello}/>
+                <Route exact path='/login' component={Login}/>
             </Switch>
         </ScrollToTop>
     </BrowserRouter>
-)
+
+}
+export default Rounter;
